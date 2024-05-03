0cf6e857932eae84c17ee2369743f5842084ae5a
Anthony Pileggi <adpileggi@gmail.com>
2024-05-03 10:56:08 -0400

    made hompage and nav componets

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import './index.css'
 
+import App from './App.jsx'
+import Home from './pages/home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+<RouterProvider router={router} />
+);
