5fd4bd52b47077802bf1daa8fdca56fabd835b19
Anthony Pileggi <adpileggi@gmail.com>
2024-05-04 16:24:16 -0400

    made array of project infromation

---
+++ Portfolio/src/utils/projects.js
@@ -0,0 +1,38 @@
+export const projects = [
+    {
+        name: 'Pizzateca Romana',
+        repoUrl: 'https://github.com/SergMalyshka/Pizzoteca-Romana',
+        deployedUrl: 'https://stormy-journey-39722-b2e17c23bcea.herokuapp.com/',
+        img: 'assets/pizzatecca-romana.jpg'
+    },
+    {
+        name: 'Rendered Recipies',
+        repoUrl: 'https://github.com/Adpileggi/agile-avengers',
+        deployedUrl: 'https://adpileggi.github.io/agile-avengers/',
+        img: 'assets/rendered-recipes.png'
+    },
+    {
+        name: 'CMS Style Blog',
+        repoUrl: 'https://github.com/Adpileggi/blog-board',
+        deployedUrl: 'https://warm-retreat-52419-3804ed1ee780.herokuapp.com/',
+        img: 'assets/cms-blog.png'
+    },
+    {
+        name: 'Note Taker App',
+        repoUrl: 'https://github.com/Adpileggi/note-taker',
+        deployedUrl: 'https://immense-forest-43018-a605e302e7d2.herokuapp.com/',
+        img: 'assets/note-taker-demo.gif'
+    },
+    {
+        name: 'Weather Dashboard',
+        repoUrl: 'https://github.com/Adpileggi/weather-dashboard',
+        deployedUrl: 'https://adpileggi.github.io/weather-dashboard/',
+        img: 'assets/weather-screenshot.png'
+    },
+    {
+        name: 'Timed Quiz',
+        repoUrl: 'https://github.com/Adpileggi/timed-quiz',
+        deployedUrl: 'https://adpileggi.github.io/timed-quiz/',
+        img: 'assets/timedquiz.png'
+    },
+]
