b0197d60b9e042c0f0ba8605333c07f85a11b686
Anthony Pileggi <adpileggi@gmail.com>
2024-05-04 18:17:56 -0400

    removed something

--- Portfolio/src/componets/Project.jsx
+++ Portfolio/src/componets/Project.jsx
@@ -4,7 +4,7 @@ function ProjectCard(props) {
             <h1>Portfolio</h1>
             {props.projects.map(project => {
                 return(
-                <div className="card" key={project.id}>
+                <div className="card mb-3" key={project.id}>
                 <img src={project.img} className="card-img-top" alt={project.alt}></img>
                 <div className="card-body">
                     <h5 key={project.id}>{project.name}</h5>
@@ -13,19 +13,7 @@ function ProjectCard(props) {
                 </div>
                 </div>
             )})}
-            <p>Something </p>
         </div>
     )
 }
 export default ProjectCard;
-
-        // {props.projects.map(project => (
-        //     <div class="card" style="width: 18rem;" key={project.id}>
-        //         <img src={project.img} class="card-img-top" alt={project.alt}></img>
-        //         <div class ="card-body">
-        //             <h5>{project.name}</h5>
-        //             <a href={project.repoUrl} class="btn btn-primary">Vist the Repo!</a>
-        //             <a href={project.deployedUrl} class="btn btn-primary">Visit this page!</a>
-        //         </div>
-        //     </div>
-        // )}
