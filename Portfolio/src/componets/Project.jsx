3c9515250646e4fc16bbd08475a7fee1858fafe9
Anthony Pileggi <adpileggi@gmail.com>
2024-05-04 18:01:33 -0400

    need to get image links working

---
+++ Portfolio/src/componets/Project.jsx
@@ -0,0 +1,31 @@
+function ProjectCard(props) {
+    return (
+        <div>
+            <h1>Portfolio</h1>
+            {props.projects.map(project => {
+                return(
+                <div className="card" style="width: 18rem;" key={project.id}>
+                <img src={project.img} className="card-img-top" alt={project.alt} key={project.id}></img>
+                <div className="card-body">
+                    <h5 key={project.id}>{project.name}</h5>
+                    <a href={project.repoUrl} className="btn btn-primary" key={project.id}>Vist the Repo!</a>
+                    <a href={project.deployedUrl} className="btn btn-primary" key={project.id}>Visit this page!</a>
+                </div>
+                </div>
+            )})}
+            <p>Something </p>
+        </div>
+    )
+}
+export default ProjectCard;
+
+        // {props.projects.map(project => (
+        //     <div class="card" style="width: 18rem;" key={project.id}>
+        //         <img src={project.img} class="card-img-top" alt={project.alt}></img>
+        //         <div class ="card-body">
+        //             <h5>{project.name}</h5>
+        //             <a href={project.repoUrl} class="btn btn-primary">Vist the Repo!</a>
+        //             <a href={project.deployedUrl} class="btn btn-primary">Visit this page!</a>
+        //         </div>
+        //     </div>
+        // )}
--- Portfolio/src/pages/Portfolio.jsx
+++ Portfolio/src/pages/Portfolio.jsx
@@ -1,9 +1,62 @@
-export default function Portfolio() {
+import ProjectCard from "../componets/Project";
+
+const projects = [
+    {
+        name: 'Pizzateca Romana',
+        repoUrl: 'https://github.com/SergMalyshka/Pizzoteca-Romana',
+        deployedUrl: 'https://stormy-journey-39722-b2e17c23bcea.herokuapp.com/',
+        img: 'assets/pizzatecca-romana.jpg',
+        alt: 'A website with an italian flag background with the title "Welcome to Pizzateca Romana"',
+        id: 1
+    },
+    {
+        name: 'Rendered Recipies',
+        repoUrl: 'https://github.com/Adpileggi/agile-avengers',
+        deployedUrl: 'https://adpileggi.github.io/agile-avengers/',
+        img: 'assets/rendered-recipes.png',
+        alt: 'A blue background with the map of the USA with pins in different regions',
+        id: 2
+    },
+    {
+        name: 'CMS Style Blog',
+        repoUrl: 'https://github.com/Adpileggi/blog-board',
+        deployedUrl: 'https://warm-retreat-52419-3804ed1ee780.herokuapp.com/',
+        img: 'assets/cms-blog.png',
+        alt: 'A homepage of a blog displaying different post',
+        id: 3
+    },
+    {
+        name: 'Note Taker App',
+        repoUrl: 'https://github.com/Adpileggi/note-taker',
+        deployedUrl: 'https://immense-forest-43018-a605e302e7d2.herokuapp.com/',
+        img: 'assets/note-taker-demo.gif',
+        alt: 'A demo showing how one can use the webage to take notes',
+        id: 4
+    },
+    {
+        name: 'Weather Dashboard',
+        repoUrl: 'https://github.com/Adpileggi/weather-dashboard',
+        deployedUrl: 'https://adpileggi.github.io/weather-dashboard/',
+        img: 'Portfolio/src/assets/weather-screenshot.png',
+        alt: 'A webpage with displaing a five day forcast',
+        id: 5
+    },
+    {
+        name: 'Timed Quiz',
+        repoUrl: 'https://github.com/Adpileggi/timed-quiz',
+        deployedUrl: 'https://adpileggi.github.io/timed-quiz/',
+        img: 'assets/timedquiz.png',
+        alt: 'A webpage displaying the start of a quiz',
+        id: 6
+    }
+]
+
+function Portfolio() {
     return (
         <div>
-            <h1>Portfolio section here</h1>
-            <p>add links to project and github?</p>
-            <p>use fecth to get my github links and info?</p>
+            <ProjectCard projects={projects} />
         </div>
     )
 }
+
+export default Portfolio;
