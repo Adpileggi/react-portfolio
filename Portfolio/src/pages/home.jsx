725dc602c5049d67d31d642933d1365ec3e5b2e9
Anthony Pileggi <adpileggi@gmail.com>
2024-05-06 12:52:29 -0400

    got text in the right spot

--- Portfolio/src/pages/home.jsx
+++ Portfolio/src/pages/home.jsx
@@ -1,15 +1,20 @@
+
 export default function Home() {
     return (
-        <div>
-        <div>
-            <h1>Anthony Pileggi</h1>
+        <div className="row">
+        <div className="col text-center align-self-center">
+        Anthony Pileggi is a fullstack JavaScript developer located in the Greater Philadelphia area. I earned a certificate in fullstack web development for the University of Pennslyvaina. My background working in hospitality provides me a unique perspecitve to resolve
         </div>
-        <div>
-            <p>Anthony Pileggi is a fullstack JavaScript developer located in the Greater Philadelphia area. I earned a certificate in fullstack web development for the University of Pennslyvaina. My background working in hospitality provides me a unique perspecitve to resolve </p>
-        </div>
-        <div>
-            <img src='ap-pic.png' alt='A recent picture of Anthony Pileggi with a cat on his shoulder' className="img-fluid"></img>
-        </div>
+
+            <div className="col">
+                <div>
+                    <h1>Anthony Pileggi</h1>
+                </div>
+                <div>
+                    <img src='ap-pic.png' alt='A recent picture of Anthony Pileggi with a cat on his shoulder' className="img-fluid"></img>
+                </div>    
+            </div>
         </div>
     )
 }
+{/* <p>Anthony Pileggi is a fullstack JavaScript developer located in the Greater Philadelphia area. I earned a certificate in fullstack web development for the University of Pennslyvaina. My background working in hospitality provides me a unique perspecitve to resolve </p> */}
