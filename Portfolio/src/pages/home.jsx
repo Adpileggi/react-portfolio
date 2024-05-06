2c978457c35c7a195c0baea14174470222534f79
Anthony Pileggi <adpileggi@gmail.com>
2024-05-06 11:11:21 -0400

    added recent photo"

--- Portfolio/src/pages/home.jsx
+++ Portfolio/src/pages/home.jsx
@@ -1,7 +1,15 @@
 export default function Home() {
     return (
         <div>
-            <h1>This is the home page or about page?</h1>
+        <div>
+            <h1>Anthony Pileggi</h1>
+        </div>
+        <div>
+            <p>Anthony Pileggi is a fullstack JavaScript developer located in the Greater Philadelphia area. I earned a certificate in fullstack web development for the University of Pennslyvaina. My background working in hospitality provides me a unique perspecitve to resolve </p>
+        </div>
+        <div>
+            <img src='ap-pic.png' alt='A recent picture of Anthony Pileggi with a cat on his shoulder' className="img-fluid"></img>
+        </div>
         </div>
     )
 }
