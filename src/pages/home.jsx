a54bf4ef521c894fbac23b1194668f6165a0378a
Anthony Pileggi <adpileggi@gmail.com>
2024-05-08 17:55:59 -0400

    fixed file structure

---
+++ src/pages/home.jsx
@@ -0,0 +1,34 @@
+const styles = {
+  padding: "10px",
+};
+
+export default function Home() {
+  return (
+    <div className="row">
+      <div>
+        <h1 className="name" style={styles}>
+          About Me
+        </h1>
+      </div>
+      <div className="col text-center align-self-center">
+        <p>
+          My name is Anthony Pileggi. I am a fullstack developer
+          located in the Greater Philadelphia area. I earned a certificate in
+          fullstack web development for the University of Pennslyvaina. My
+          background working in hospitality provides me a unique perspecitve to
+          resolve solve complex problems.
+        </p>
+      </div>
+
+      <div className="col">
+        <div>
+          <img
+            src="ap-pic-resize.png"
+            alt="A recent picture of Anthony Pileggi with a cat on his shoulder"
+            className="img-fluid"
+          ></img>
+        </div>
+      </div>
+    </div>
+  );
+}
